0b501e23cba0a054f7dc11adc6a684d0b6a88d2b
ember arlynx <ember@lunar.town>
2023-01-14 21:43:12 -0500

    The code seems to work!

--- Assets/HRTextUpdater.cs
+++ Assets/HRTextUpdater.cs
@@ -2,33 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Events;
+
 public class HRTextUpdater : MonoBehaviour
 {
 
-    public TextMeshProUGUI labelElement;
+    public TMP_Text labelElement;
     string textTemplate;
     public OurSingularityHappenings sensor_data;
+    float hr, hrv, stress_level;
 
     // Start is called before the first frame update
     void Start()
     {
         textTemplate = labelElement.text;
-        sensor_data.hr.AddListener(hr =>
-        {
-            UpdateTemplate("{hr}", hr);
-        });
-        sensor_data.hrv.AddListener(hrv =>
-        {
-            UpdateTemplate("{hrv}", hrv);
-        });
-        sensor_data.stress_level.AddListener(sl =>
-        {
-            UpdateTemplate("{sl}", sl);
-        });
     }
 
-    private void UpdateTemplate(string metavariable, float value)
+    private void Update()
     {
-        labelElement.text = textTemplate.Replace(metavariable, value.ToString());
+        string message = sensor_data.CurrentMsg;
+        if (sensor_data.MsgReceived)
+        {
+            sensor_data.MsgReceived = false;
+            Debug.LogFormat("Message recieved from device: {1} bytes: {0}", message.Length, message);
+            if (message.StartsWith("DATA:"))
+            {
+                var tabloc = message.IndexOf('\t');
+                var payload = message[tabloc..];
+                var tag = message[0..tabloc];
+                float payload_value = float.Parse(payload);
+
+                switch (tag)
+                {
+                    case "DATA:HR:":
+                        hr = payload_value;
+                        break;
+                    case "DATA:HRV:":
+                        hrv = payload_value;
+                        break;
+                    case "DATA:SL:":
+                        stress_level = payload_value;
+                        break;
+                    default:
+                        Debug.LogErrorFormat("Argh! {0} is absolutely not one of our payload types!", tag);
+                        return;
+                }
+                UpdateTemplate();
+            }
+        }
+
+    }
+    private void UpdateTemplate()
+    {
+        labelElement.text = textTemplate.Replace("{hr}", hr.ToString()).Replace("{hrv}", hrv.ToString()).Replace("{sl}", stress_level.ToString());
     }
 }
